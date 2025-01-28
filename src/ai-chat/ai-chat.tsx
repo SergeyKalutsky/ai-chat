9a407ba45d65a1b3a1ee2a977c34f140ab87f7a7
TImurJaNaYeV <tima.janayev@gmail.com>
2025-01-28 17:45:40 +0500

    Hover typ thing

--- src/ai-chat/ai-chat.tsx
+++ src/ai-chat/ai-chat.tsx
@@ -3,7 +3,7 @@ import { FaRobot } from "react-icons/fa6";
 export default function AiChatIcon() {
   return (
     <>
-      <div className="fixed bottom-4 right-4 z-50 text-4xl">
+      <div className="fixed bottom-4 right-4 z-50 text-4xl cursor-pointer hover:border-2 border-gray-500 rounded-full p-2">
         <FaRobot />
       </div>
     </>
