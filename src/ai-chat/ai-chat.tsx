42e66b5c08b9845b5c27668afaf9a06631e0679b
User <User@WIN-EQO5781G9M2>
2025-01-28 15:36:51 +0300

    fixed a bit

--- src/ai-chat/ai-chat.tsx
+++ src/ai-chat/ai-chat.tsx
@@ -1,11 +1,9 @@
 import { FaRobot } from "react-icons/fa6";
 
-
-
 export default function AiChatIcon() {
   return (
     <>
-      <div>
+      <div className="fixed bottom-4 right-4 z-50 text-4xl">
         <FaRobot />
       </div>
     </>
